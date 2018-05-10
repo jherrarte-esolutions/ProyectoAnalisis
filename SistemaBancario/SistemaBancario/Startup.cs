321a027c02854e5cf104517c3fbfa835e17cedb6
JONNYH\Jonny Chavez <jonnychaavez@gmail.com>
2018-05-10 15:26:26 -0600

    Proyecto de analisis

---
+++ SistemaBancario/SistemaBancario/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SistemaBancario.Startup))]
+namespace SistemaBancario
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
